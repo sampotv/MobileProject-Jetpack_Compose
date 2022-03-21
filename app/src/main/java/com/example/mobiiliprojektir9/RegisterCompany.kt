ee2f1eb3dc4e92d36aecfb056f7a00eb61ad7063
hsniemi <78792539+hsniemi@users.noreply.github.com>
2022-03-21 18:49:08 +0200

    Muutoksia rekisteröinti-näkymään

--- app/src/main/java/com/example/mobiiliprojektir9/RegisterCompany.kt
+++ app/src/main/java/com/example/mobiiliprojektir9/RegisterCompany.kt
@@ -2,16 +2,24 @@ package com.example.mobiiliprojektir9
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mobiiliprojektir9.ui.theme.MobiiliprojektiR9Theme
 import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Preview
 @Composable
 fun RegisterCompany(){
@@ -26,6 +34,7 @@ fun RegisterCompany(){
     }
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         scaffoldState = scaffoldState
@@ -51,6 +60,10 @@ fun RegisterCompany(){
                 onValueChange = {
                     emailState = it
                 },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email,imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}),
+                shape = RoundedCornerShape(8.dp),
                 modifier = Modifier.fillMaxWidth()
             )
             OutlinedTextField(
@@ -61,7 +74,11 @@ fun RegisterCompany(){
                 onValueChange = {
                     passwordState = it
                 },
-                modifier = Modifier.fillMaxWidth()
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password,imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}),
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
             )
             OutlinedTextField(
                 value = companyState,
--- app/src/main/java/com/example/mobiiliprojektir9/RegisterDriver.kt
+++ app/src/main/java/com/example/mobiiliprojektir9/RegisterDriver.kt
@@ -2,16 +2,25 @@ package com.example.mobiiliprojektir9
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mobiiliprojektir9.ui.theme.MobiiliprojektiR9Theme
 import kotlinx.coroutines.launch
 
+@ExperimentalComposeUiApi
 @Preview
 @Composable
 fun RegisterDriver(){
@@ -26,6 +35,7 @@ fun RegisterDriver(){
     }
     val scaffoldState = rememberScaffoldState()
     val scope = rememberCoroutineScope()
+    val keyboardController = LocalSoftwareKeyboardController.current
     Scaffold(
         modifier = Modifier.fillMaxSize(),
         scaffoldState = scaffoldState
@@ -51,7 +61,11 @@ fun RegisterDriver(){
                 onValueChange = {
                     emailState = it
                 },
-                modifier = Modifier.fillMaxWidth()
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email,imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}),
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
             )
             OutlinedTextField(
                 value = passwordState,
@@ -61,7 +75,11 @@ fun RegisterDriver(){
                 onValueChange = {
                     passwordState = it
                 },
-                modifier = Modifier.fillMaxWidth()
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password,imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}),
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
             )
             OutlinedTextField(
                 value = companyState,
@@ -71,7 +89,11 @@ fun RegisterDriver(){
                 onValueChange = {
                     companyState = it
                 },
-                modifier = Modifier.fillMaxWidth()
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+                keyboardActions = KeyboardActions(
+                    onDone = {keyboardController?.hide()}),
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
             )
             Spacer(modifier = Modifier.height(24.dp))
             Button(onClick = {
