9e339170380288dc11e6f2ed44abd04e9be99953
hsniemi <78792539+hsniemi@users.noreply.github.com>
2022-03-23 13:53:08 +0200

    firebase liitetty

---
+++ app/src/main/java/com/example/mobiiliprojektir9/DriverData.kt
@@ -0,0 +1,11 @@
+package com.example.mobiiliprojektir9
+
+import java.util.*
+
+data class DriverData(
+    var email: String = "",
+    var password: String = "",
+    var company: String = "",
+    var driverId: String = ""
+) {
+}
--- app/src/main/java/com/example/mobiiliprojektir9/RegisterDriver.kt
+++ app/src/main/java/com/example/mobiiliprojektir9/RegisterDriver.kt
@@ -1,5 +1,10 @@
 package com.example.mobiiliprojektir9
 
+import android.os.Bundle
+import android.service.controls.ControlsProviderService.TAG
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -11,19 +16,39 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.mobiiliprojektir9.ui.theme.MobiiliprojektiR9Theme
-import kotlinx.coroutines.launch
+import com.google.firebase.firestore.FirebaseFirestore
 
+class RegisterDriver : ComponentActivity() {
+
+    @OptIn(ExperimentalComposeUiApi::class)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            MobiiliprojektiR9Theme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    RegisterAsDriver()
+                }
+            }
+        }
+    }
+}
 @ExperimentalComposeUiApi
 @Preview
 @Composable
-fun RegisterDriver(){
+fun RegisterAsDriver(){
+    val context = LocalContext.current
     var emailState by remember {
         mutableStateOf("")
     }
@@ -33,78 +58,99 @@ fun RegisterDriver(){
     var companyState by remember {
         mutableStateOf("")
     }
-    val scaffoldState = rememberScaffoldState()
+    val isSaved = remember { mutableStateOf(false)}
     val scope = rememberCoroutineScope()
     val keyboardController = LocalSoftwareKeyboardController.current
-    Scaffold(
-        modifier = Modifier.fillMaxSize(),
-        scaffoldState = scaffoldState
+    Column(
+        modifier = Modifier
+            .padding(24.dp)
+            .fillMaxSize()
+            .background(Color.White),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
     ) {
-        Column(
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxSize()
-                .background(Color.White),
-            horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Top
-        ) {
-            Text(
-                text = "Rekisteröidy kuljettajana",
-                style = MaterialTheme.typography.h5
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            OutlinedTextField(
-                value = emailState,
-                label = {
-                    Text("Email")
-                },
-                onValueChange = {
-                    emailState = it
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email,imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
-                    onDone = {keyboardController?.hide()}),
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
-            )
-            OutlinedTextField(
-                value = passwordState,
-                label = {
-                    Text("Salasana")
-                },
-                onValueChange = {
-                    passwordState = it
-                },
-                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password,imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
-                    onDone = {keyboardController?.hide()}),
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
-            )
-            OutlinedTextField(
-                value = companyState,
-                label = {
-                    Text("Yritys")
-                },
-                onValueChange = {
-                    companyState = it
-                },
-                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
-                keyboardActions = KeyboardActions(
-                    onDone = {keyboardController?.hide()}),
-                modifier = Modifier.fillMaxWidth(),
-                shape = RoundedCornerShape(8.dp)
-            )
-            Spacer(modifier = Modifier.height(24.dp))
-            Button(onClick = {
-                scope.launch {
-                    scaffoldState.snackbarHostState.showSnackbar("$emailState, $passwordState, $companyState")
-                }
-            }) {
-                Text("Rekisteröidy")
+        Text(
+            text = "Rekisteröidy kuljettajana",
+            style = MaterialTheme.typography.h5
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        OutlinedTextField(
+            value = emailState,
+            label = {
+                Text("Email")
+            },
+            onValueChange = {
+                emailState = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Email,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { keyboardController?.hide() }),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+        )
+        OutlinedTextField(
+            value = passwordState,
+            label = {
+                Text("Salasana")
+            },
+            onValueChange = {
+                passwordState = it
+            },
+            keyboardOptions = KeyboardOptions(
+                keyboardType = KeyboardType.Password,
+                imeAction = ImeAction.Done
+            ),
+            keyboardActions = KeyboardActions(
+                onDone = { keyboardController?.hide() }),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+        )
+        OutlinedTextField(
+            value = companyState,
+            label = {
+                Text("Yritys")
+            },
+            onValueChange = {
+                companyState = it
+            },
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Done),
+            keyboardActions = KeyboardActions(
+                onDone = { keyboardController?.hide() }),
+            modifier = Modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(8.dp)
+        )
+        Spacer(modifier = Modifier.height(24.dp))
+        Button(onClick = {
+            var driverData = DriverData().apply {
+                email = emailState.toString()
+                password = passwordState.toString()
+                company = companyState.toString()
             }
+            saveDriverData(driverData)
+//            Toast.makeText(
+//                context,
+//                "$emailState, $passwordState, $companyState",
+//                Toast.LENGTH_LONG
+//            ).show()
+        }) {
+            Text("Rekisteröidy")
         }
     }
+}
+
+fun saveDriverData(driverData: DriverData) {
+    val db = FirebaseFirestore.getInstance()
+    db.collection("drivers")
+        .add(driverData)
+        .addOnSuccessListener { documentReference ->
+            Log.d(TAG, "DocumentSnapshot added with ID: ${documentReference.id}")
+        }
+        .addOnFailureListener { e ->
+            Log.w(TAG, "Error adding document", e)
+        }
 }
 
 //@Preview()
